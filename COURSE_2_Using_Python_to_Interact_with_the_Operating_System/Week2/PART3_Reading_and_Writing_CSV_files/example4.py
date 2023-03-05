20f435a21377d4468238e1d8875884856c41d38b
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-03-05 02:34:35 -0600

    Forth example, write a csv file using dictionaries

---
+++ COURSE_2_Using_Python_to_Interact_with_the_Operating_System/Week2/PART3_Reading_and_Writing_CSV_files/example4.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+
+import os
+import time
+import csv
+
+users = [{"name":"Sol Mansi", "username":"solm", "department":"IT infrastructure"}, {"name":"Lio Nelson", "username":"lion", "department":"User Experience Research"}, {"name":"Charlie Grey", "username":"greyc", "department":"Development"}]
+
+keys = ["name", "username", "department"]
+
+with open("by_department.csv", "w") as by_department:
+	writer = csv.DictWriter(by_department, fieldnames=keys)
+	writer.writeheader()
+	writer.writerows(users)
+
+os.system("cat by_department.csv")
+	
