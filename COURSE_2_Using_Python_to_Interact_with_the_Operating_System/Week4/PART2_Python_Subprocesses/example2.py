3b8028caed50ac093ef7624f96e9a4a007df5c15
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-05-27 21:06:20 -0600

    Add examples from part 2

---
+++ COURSE_2_Using_Python_to_Interact_with_the_Operating_System/Week4/PART2_Python_Subprocesses/example2.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+
+import subprocess
+
+result = subprocess.run(["host", "8.8.8.8"], capture_output=True)
+print(result.returncode)
+print(result.stdout)
+print(result.stdout.decode().split())
+print()
+result = subprocess.run(["rm", "does_not_exist"], capture_output=True)
+print(result.returncode)
+print(result.stdout)
+print(result.stderr)
