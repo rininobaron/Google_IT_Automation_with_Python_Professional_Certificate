7413e81014afb0c7655508afa665dcef6530d032
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-09-26 15:50:19 -0600

    Add a check_rebbot function

--- COURSE_3_Introduction_to_Git_and_GitHub/PART_3_Using_Git/all_checks.py
+++ COURSE_3_Introduction_to_Git_and_GitHub/PART_3_Using_Git/all_checks.py
@@ -1,6 +1,12 @@
-#! /usr/bin/env/ python3
+#! /usr/bin/env python3
+
+import os
+
+def check_reboot():
+	"""Return True if the computer has a pending reboot."""
+	return os.path.exist("/run/reboot-required")
 
 def main():
-	pass:
+	pass
 
 main()
